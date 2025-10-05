7523f5dc464d2b63a8341d42706944c3a8b3d247
KRYOGA <k.ryo.96@icloud.com>
2025-10-05 23:26:56 +0900

    参照ファイルの変更
    index.htmlファイルを参照するのではなく、最初に開いたサイトはゼミの概要ページがくるように変更

--- my_node_server/index.js
+++ my_node_server/index.js
@@ -6,21 +6,16 @@ const app = express();
 const port = 8000;
 
 // 【追加】'public' ディレクトリ内のファイルを静的ファイルとして公開する
-// これにより、 http://localhost:3000/index.html でファイルにアクセス可能になる
+// これにより、 http://localhost:8000/view.html でファイルにアクセス可能になる
 app.use(express.static('public'));
 
 
 // ルートパス (/) へのGETリクエストに対する処理
 app.get('/', (req, res) => {
-  // レスポンスとして文字列を送信
-  // ★ ここを直接HTMLファイルを返すように修正します ★
-  // res.send('Hello World! Node.js Server is running!'); 
   
-  // サーバーの実行ファイルからの相対パスで index.html を送信
-  res.sendFile(__dirname + '/public/index.html');
+  // サーバーの実行ファイルからの相対パスで view.html を送信
+  res.sendFile(__dirname + '/public/view.html');
   
-  // 備考: 上記で express.static('public') を設定しているため、
-  // res.sendFile() の代わりに res.redirect('/index.html') も機能します。
 });
 
 
